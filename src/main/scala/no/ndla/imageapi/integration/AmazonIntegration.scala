b0fae65ed59f1ad8a1c91c38b46f7d866496d038
Kenneth Stigen <kenneth.stigen@delfidata.no>
2015-08-03 15:56:26 +0200

    NDLA-9: Dyttet inn tilgangsdetaljer i koden

--- src/main/scala/no/ndla/imageapi/integration/AmazonIntegration.scala
+++ src/main/scala/no/ndla/imageapi/integration/AmazonIntegration.scala
@@ -1,6 +1,6 @@
 package no.ndla.imageapi.integration
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
@@ -12,14 +12,17 @@ object AmazonIntegration {
   val ImageBucketName = "ndla-image"
   val ImageMetaName = "ndla-image-meta"
 
+  val ImageAPIAccessKey = "AKIAJYVWL6WV5PL3XCEQ"
+  val ImageAPISecretAccessKey = "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"
+
   def getImageMeta(): AmazonImageMeta = {
-    val dbClient = new AmazonDynamoDBClient(new ProfileCredentialsProvider())
+    val dbClient = new AmazonDynamoDBClient(new BasicAWSCredentials(ImageAPIAccessKey, ImageAPISecretAccessKey))
     val dynamoDb = new DynamoDB(dbClient)
     new AmazonImageMeta(ImageMetaName, dbClient, dynamoDb)
   }
 
   def getImageBucket(): AmazonImageBucket = {
-    val s3Client = new AmazonS3Client(new ProfileCredentialsProvider())
+    val s3Client = new AmazonS3Client(new BasicAWSCredentials(ImageAPIAccessKey, ImageAPISecretAccessKey))
     s3Client.setRegion(Region.getRegion(Regions.EU_WEST_1))
     new AmazonImageBucket(ImageBucketName, s3Client)
   }
