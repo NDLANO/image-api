02de027f78167ff97e798388d19ca8eadf938bab
Kenneth Stigen <kenneth.stigen@delfidata.no>
2015-08-06 10:33:08 +0200

    NDLA-9: Lagt til swagger-dok på domeneklasser

--- src/main/scala/model/Image.scala
+++
@@ -1,7 +0,0 @@
-package model
-
-case class ImageMetaInformation(id:String, title:String, images:ImageVariants, copyright:Copyright, tags:List[String])
-case class ImageVariants(small: Image, full: Image)
-case class Image(url:String, size:String)
-case class Copyright(license:String, origin:String, authors:List[Author])
-case class Author(`type`:String, name:String)
