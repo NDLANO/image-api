c02155fb151dc794061c50b1172ab94672c38497
Kenneth Stigen <kenneth.stigen@delfidata.no>
2015-08-06 09:03:59 +0200

    NDLA-9: Lagt til dok på utydelige ImageStorage metoder

--- src/main/scala/no/ndla/imageapi/business/ImageStorage.scala
+++ src/main/scala/no/ndla/imageapi/business/ImageStorage.scala
@@ -5,8 +5,28 @@ import java.io.InputStream
 import model.ImageMetaInformation
 
 trait ImageStorage {
+  /**
+   * Retrieves a handle to the image from storage.
+   *
+   * @param imageKey The key name the image is stored as
+   * @return Optionally a tuple with mimetype and inputstream to the actual image
+   */
   def get(imageKey: String): Option[(String, InputStream)]
+
+  /**
+   * Uploads an image to storage.
+   * Example usage:
+   * - The image is located at /home/user/images/myImage.jpg
+   * - ImageMetaInformation holds myImage.jpg
+   * - imageDirectory should be set to /home/user/images/
+   *
+   * TODO: This method should be rewritten
+   *
+   * @param imageMetaInformation The meta-information describing the image.
+   * @param imageDirectory The path (on the local host) to where the image resides, excluding the image name.
+   */
   def upload(imageMetaInformation: ImageMetaInformation, imageDirectory: String)
+
   def contains(imageMetaInformation: ImageMetaInformation): Boolean
   def exists(): Boolean
   def create()
