777d057ba763eef561a4a8d8326c9adf6633c877
Kenneth Stigen <kenneth.stigen@delfidata.no>
2015-08-18 15:04:34 +0200

    NDLA-31: Testklient gir søkeresultat som tekst

---
+++ src/main/webapp/image-api/testclient/js/testclient.js
@@ -0,0 +1,21 @@
+function search() {
+    var tagString = $('#tags').val();
+    var minSize = $('#minSize').val();
+    var searchUrl = "/images"
+
+    if(tagString) {
+        searchUrl = searchUrl.concat("?tags=", tagString)
+    }
+    if(minSize) {
+        searchUrl = searchUrl.concat("&minimumSize=", minSize)
+    }
+
+    var request = window.superagent;
+    request.get(searchUrl).end(
+        function(err, res) {
+            $('#searchresults').empty();
+            $('#searchresults').append(res.text);
+            console.log(res.text)
+        }
+    )
+}
