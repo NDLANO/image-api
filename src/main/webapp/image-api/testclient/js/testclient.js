fd0c867d599ff3c95852ce1f53932f8cdceccf8f
Kenneth Stigen <kenneth.stigen@delfidata.no>
2015-08-19 13:44:02 +0200

    NDLA-31: Viser bilder og data ved klikk

--- src/main/webapp/image-api/testclient/js/testclient.js
+++ src/main/webapp/image-api/testclient/js/testclient.js
@@ -4,11 +4,57 @@ function searchIfEnter(event){
     }
 }
 
+function showImage(jsonData) {
+    $('#imagedetail').show();
+
+    $('#imagetitle').empty();
+    $.each(jsonData["titles"], function(index, element) {
+        $('#imagetitle').append(element["title"], "<br/>");
+    });
+
+    $('#imagesize').empty().append(jsonData["images"]["full"]["size"]);
+    $('#license').empty().append(jsonData["copyright"]["license"]);
+
+    var origin = '<a href="' + jsonData["copyright"]["origin"] + '" target="_blank">' + jsonData["copyright"]["origin"] + '</a>';
+    $('#origin').empty().append(origin);
+
+    $('#authors').empty();
+    var numElements = jsonData["copyright"]["authors"].length;
+
+    $.each(jsonData["copyright"]["authors"], function(index, element){
+        $('#authors').append(element["name"]);
+        if(index < numElements-1){
+            $('#authors').append(", ");
+        }
+    });
+
+    $('#imagetags').empty();
+    $.each(jsonData["tags"], function(index, element){
+        $('#imagetags').append('<span class="tag">' + element["tag"] + '</span>');
+    });
+
+    var fullsizeUrl = jsonData["images"]["full"]["url"];
+
+    $('#imageview').empty().append('<a href="' + fullsizeUrl + '" target="_blank"><img src="' + fullsizeUrl + '"/></a>');
+}
+
+function loadImage(url){
+    var request = window.superagent;
+    request.get(url).end(
+        function(err, res) {
+            if(res.ok){
+                showImage(res.body);
+            } else {
+                console.log("Dette gikk ikke bra...");
+            }
+        }
+    )
+}
+
 function searchOk(jsonData){
     $('#searchresults').empty();
     $.each(jsonData, function(index, element) {
-        var previewImg = "<a href='#'><img src='" + element["previewUrl"] + "'/></a>"
-
+        var previewImg = '<a href="#" onclick=\'loadImage("' + element["metaUrl"] + '");\'><img src="' + element["previewUrl"] + '"/></a>';
         $('#searchresults').append(previewImg);
     });
 }
@@ -25,6 +71,8 @@ function search() {
     if(minSize) {
         searchUrl = searchUrl.concat("&minimumSize=", minSize)
     }
+
+    $('#imagedetail').hide();
 
     var request = window.superagent;
     request.get(searchUrl).end(
