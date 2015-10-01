6053206ff4a52e8a9d5c4bab9cba86f3ab4aa1d8
Kenneth Stigen <kes@knowit.no>
2015-10-01 10:06:59 +0200

    NDLA-46: Endret navn

--- src/main/scala/no/ndla/imageapi/batch/ImageSearchIndexer.scala
+++ src/main/scala/no/ndla/imageapi/batch/ImageSearchIndexer.scala
@@ -4,15 +4,15 @@ import no.ndla.imageapi.integration.AmazonIntegration
 
 object ImageSearchIndexer {
 
-  val dbMeta = AmazonIntegration.getImageMeta()
-  val searchMeta = AmazonIntegration.getSearchMeta()
+  val meta = AmazonIntegration.getImageMeta()
+  val search = AmazonIntegration.getSearchMeta()
 
   def main(args: Array[String]) {
     val start = System.currentTimeMillis()
 
-    searchMeta.createIndex()
-    dbMeta.foreach(imageMeta => {
-      searchMeta.indexDocument(imageMeta)
+    search.createIndex()
+    meta.foreach(imageMeta => {
+      search.indexDocument(imageMeta)
       println(s"Indexed document with id: ${imageMeta.id}.")
     })
 
