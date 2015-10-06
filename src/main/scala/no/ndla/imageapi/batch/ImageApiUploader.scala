65e4cac7f9b20c1210dff9206f77defcc461b0fa
Kenneth Stigen <kes@knowit.no>
2015-10-06 10:10:36 +0200

    NDLA-38: Lagt på feilhåndtering og logging

--- src/main/scala/no/ndla/imageapi/batch/ImageApiUploader.scala
+++ src/main/scala/no/ndla/imageapi/batch/ImageApiUploader.scala
@@ -16,11 +16,13 @@ object ImageApiUploader {
 
   def main(args: Array[String]) {
 
-    new ImageApiUploader(maxUploads = 600,
-      imageMetaFile = "/Users/kes/sandboxes/ndla/data-dump/20151005_0900/imagemeta.csv",
-      licensesFile = "/Users/kes/sandboxes/ndla/data-dump/20151005_0900/license.csv",
-      authorsFile = "/Users/kes/sandboxes/ndla/data-dump/20151005_0900/authors.csv",
-      originFile = "/Users/kes/sandboxes/ndla/data-dump/20151005_0900/origin.csv")
+
+
+    new ImageApiUploader(maxUploads = 300,
+      imageMetaFile = "/Users/kes/sandboxes/ndla/data-dump/20151006_0957/imagemeta.csv",
+      licensesFile = "/Users/kes/sandboxes/ndla/data-dump/20151006_0957/license.csv",
+      authorsFile = "/Users/kes/sandboxes/ndla/data-dump/20151006_0957/authors.csv",
+      originFile = "/Users/kes/sandboxes/ndla/data-dump/20151006_0957/origin.csv")
       .uploadImages()
   }
 
@@ -32,47 +34,51 @@ object ImageApiUploader {
  *                   
  * @param imageMetaFile Fil som inneholder metainformasjon for bilder.
  *                      Kan produseres av følgende sql:
-                        select n.nid, n.tnid, NULLIF(n.language,''), n.title,
-                              NULLIF(cti.field_alt_text_value, ''),
-                              from_unixtime(n.changed),
-                              f.filepath as original,
-                              f.filemime as original_mime,
-                              f.filesize as original_size,
-                              f2.filepath as thumb,
-                              f2.filemime as thumb_mime,
-                              f2.filesize as thumb_size
-                        from node n
-                          left join image i on (n.nid = i.nid)
-                          left join image i2 on (n.nid = i2.nid)
-                          left join files f on (i.fid = f.fid)
-                          left join files f2 on (i2.fid = f2.fid)
-                          left join content_type_image cti on (n.nid = cti.nid)
-                        where n.type = "image" and n.status = 1
-                          and i.image_size = "_original"
-                          and i2.image_size = "thumbnail"
-                        order by n.nid desc limit 400;
- *
+ *                       select n.nid, n.tnid, NULLIF(n.language,''), n.title,
+ *                         NULLIF(cti.field_alt_text_value, ''),
+ *                         from_unixtime(n.changed),
+ *                         f.filepath as original,
+ *                         f.filemime as original_mime,
+ *                         f.filesize as original_size,
+ *                         f2.filepath as thumb,
+ *                         f2.filemime as thumb_mime,
+ *                         f2.filesize as thumb_size
+ *                       from node n
+ *                         left join image i on (n.nid = i.nid)
+ *                         left join image i2 on (n.nid = i2.nid)
+ *                         left join files f on (i.fid = f.fid)
+ *                         left join files f2 on (i2.fid = f2.fid)
+ *                         left join content_type_image cti on (n.nid = cti.nid)
+ *                       where n.type = "image" and n.status = 1
+ *                         and i.image_size = "_original"
+ *                         and i2.image_size = "thumbnail"
+ *                       order by n.nid desc limit 400;
  *
  * @param licensesFile Fil som inneholder lisensinformasjon om bilder
  *                     Kan produseres med følgende sql:
- *                     select n.nid, cc.license from node n
- *                     left join creativecommons_lite cc on (n.nid = cc.nid)
- *                     where n.type = "image" and n.status = 1;
+ *                     SELECT n.nid, cc.license from node n
+ *                       left join creativecommons_lite cc on (n.nid = cc.nid)
+ *                     WHERE n.type = "image"
+ *                       AND n.status = 1
+ *                       AND NULLIF(cc.license,'') is not null;
  *
  * @param authorsFile Fil som inneholder authors for bilder.
  *                    Kan produseres av følgende sql:
  *                    SELECT n.nid AS image_nid, td.name AS author_type, person.title AS author FROM node n
- *                    LEFT JOIN ndla_authors na ON n.vid = na.vid
- *                    LEFT JOIN term_data td ON na.tid = td.tid
- *                    LEFT JOIN node person ON person.nid = na.person_nid
- *                    WHERE n.type = 'image' and n.status = 1 LIMIT 80000;
+ *                      LEFT JOIN ndla_authors na ON n.vid = na.vid
+ *                      LEFT JOIN term_data td ON na.tid = td.tid
+ *                      LEFT JOIN node person ON person.nid = na.person_nid
+ *                    WHERE n.type = 'image' and n.status = 1
+ *                      AND person.title is not null
+ *                      AND td.name is not null
+ *                    LIMIT 80000;
  *
  * @param originFile Fil som inneholder origins for bilder.
  *                   Kan produseres av følgende sql:
  *                   select n.nid, url.field_url_url from node n
  *                   left join content_field_url url ON url.vid = n.vid
  *                   where n.type = 'image' and n.status = 1 and url.field_url_url is not null
- *                   limit 40000
+ *                   limit 40000;
  *
  *
  */
@@ -134,53 +140,74 @@ class ImageApiUploader(maxUploads:Int = 1, imageMetaFile: String, licensesFile: 
     .toMap
 
   val imageAuthor = Source.fromFile(authorsFile)("UTF-8").getLines
-    .map(line => toImageAuthor(line))
-    .map(author => author.nid -> author)
-    .toList.groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
+    .map(line => toImageAuthor(line)).collect{case Some(x) => x}
+    .map(author => author.nid -> author).toList
+    .groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
 
   val imageStorage = AmazonIntegration.getImageStorageDefaultCredentials();
   val imageMetaStore = AmazonIntegration.getImageMeta()
 
+  val failed = scala.collection.mutable.Map[String, String]()
+  var inserted = 0
+  var updated = 0
+
   def uploadImages() = {
+    val start = System.currentTimeMillis
     imageMetaWithoutTranslations.take(maxUploads).foreach(upload(_))
+
+    println(s"Inserted $inserted, updated $updated in ${System.currentTimeMillis - start}ms.")
+    if(!failed.isEmpty){
+      println("The following failed:")
+      failed.foreach(elem => println(s"${elem._1}:   ${elem._2}"))
+    }
   }
 
   def upload(imageMeta: ImageMeta) = {
-    val license = imageLicense.getOrElse(imageMeta.nid, ImageLicense("", "")).license
-    val origin = imageOrigin.getOrElse(imageMeta.nid, ImageOrigin("", "")).origin
-    val imageAuthors = imageAuthor.getOrElse(imageMeta.nid, List())
-    val sourceUrlFull = DownloadUrlPrefix + imageMeta.originalFile
-    val sourceUrlThumb = DownloadUrlPrefix + imageMeta.thumbFile
-    val tags = Tags.forImage(imageMeta.nid)
+    val start = System.currentTimeMillis
+    try {
+      val license = imageLicense.getOrElse(imageMeta.nid, ImageLicense("", "")).license
+      val origin = imageOrigin.getOrElse(imageMeta.nid, ImageOrigin("", "")).origin
+      val imageAuthors = imageAuthor.getOrElse(imageMeta.nid, List())
+      val sourceUrlFull = DownloadUrlPrefix + imageMeta.originalFile
+      val sourceUrlThumb = DownloadUrlPrefix + imageMeta.thumbFile
+      val tags = Tags.forImage(imageMeta.nid)
 
-    val thumbKey = imageMeta.thumbFile.replace("sites/default/files/images/", "thumbs/")
-    val thumb = Image(thumbKey, imageMeta.thumbSize.toInt, imageMeta.thumbMime)
+      val thumbKey = imageMeta.thumbFile.replace("sites/default/files/images/", "thumbs/")
+      val thumb = Image(thumbKey, imageMeta.thumbSize.toInt, imageMeta.thumbMime)
 
-    val fullKey = imageMeta.originalFile.replace("sites/default/files/images/", "full/")
-    val full = Image(fullKey, imageMeta.originalSize.toInt, imageMeta.originalMime)
+      val fullKey = imageMeta.originalFile.replace("sites/default/files/images/", "full/")
+      val full = Image(fullKey, imageMeta.originalSize.toInt, imageMeta.originalMime)
 
-    val authors = imageAuthors.map(ia => Author(ia.typeAuthor, ia.name))
-    val copyright = Copyright(licenseToLicenseDefinitionsMap.getOrElse(license, License(license, license, None)), origin, authors)
+      val authors = imageAuthors.map(ia => Author(ia.typeAuthor, ia.name))
+      val copyright = Copyright(licenseToLicenseDefinitionsMap.getOrElse(license, License(license, license, None)), origin, authors)
 
 
-    var titles = List(ImageTitle(imageMeta.title, languageToISOMap.get(imageMeta.language)))
-    var alttexts = List(ImageAltText(imageMeta.alttext, languageToISOMap.get(imageMeta.language)))
-    translations.get(imageMeta.nid).foreach(_.foreach(translation => {
-      titles = ImageTitle(translation.title, languageToISOMap.get(translation.language)) :: titles
-      alttexts = ImageAltText(translation.alttext, languageToISOMap.get(translation.language)) :: alttexts
-    }))
+      var titles = List(ImageTitle(imageMeta.title, languageToISOMap.get(imageMeta.language)))
+      var alttexts = List(ImageAltText(imageMeta.alttext, languageToISOMap.get(imageMeta.language)))
+      translations.get(imageMeta.nid).foreach(_.foreach(translation => {
+        titles = ImageTitle(translation.title, languageToISOMap.get(translation.language)) :: titles
+        alttexts = ImageAltText(translation.alttext, languageToISOMap.get(translation.language)) :: alttexts
+      }))
 
-    val imageMetaInformation = ImageMetaInformation("0", titles, alttexts, ImageVariants(Option(thumb), Option(full)), copyright, tags)
+      val imageMetaInformation = ImageMetaInformation("0", titles, alttexts, ImageVariants(Option(thumb), Option(full)), copyright, tags)
 
-    if(!imageMetaStore.containsExternalId(imageMeta.nid)) {
-      if(!imageStorage.contains(thumbKey)) imageStorage.uploadFromUrl(thumb, thumbKey, sourceUrlThumb)
-      if(!imageStorage.contains(fullKey)) imageStorage.uploadFromUrl(full, fullKey, sourceUrlFull)
+      if(!imageMetaStore.containsExternalId(imageMeta.nid)) {
+        if(!imageStorage.contains(thumbKey)) imageStorage.uploadFromUrl(thumb, thumbKey, sourceUrlThumb)
+        if(!imageStorage.contains(fullKey)) imageStorage.uploadFromUrl(full, fullKey, sourceUrlFull)
 
-      imageMetaStore.insert(imageMetaInformation, imageMeta.nid)
-      println("NEW-UPLOAD:  " + imageMeta.nid + " (" + imageMeta.title  + ") with license " + license + " and authors " + authors.map(_.name) + ", full: " + sourceUrlFull + ", thumb: " + sourceUrlThumb + " with tags " + tags)
-    } else {
-      imageMetaStore.update(imageMetaInformation, imageMeta.nid)
-      println("EXISTING-UPDATE:  " + imageMeta.nid + " (" + imageMeta.title  + ") with license " + license + " and authors " + authors.map(_.name) + ", full: " + sourceUrlFull + ", thumb: " + sourceUrlThumb + " with tags " + tags)
+        imageMetaStore.insert(imageMetaInformation, imageMeta.nid)
+        inserted += 1
+        println("INSERT: " + imageMeta.nid + " (" + imageMeta.title  + ", " + sourceUrlFull + ") -- " + (System.currentTimeMillis - start) + "ms")
+      } else {
+        imageMetaStore.update(imageMetaInformation, imageMeta.nid)
+        updated += 1
+        println("UPDATE: " + imageMeta.nid + " (" + imageMeta.title  + ", " + sourceUrlFull + ") -- " + (System.currentTimeMillis - start) + "ms")
+      }
+    } catch {
+      case e: Exception  => {
+        e.printStackTrace()
+        failed += imageMeta.nid -> s"${imageMeta.nid} failed after ${System.currentTimeMillis - start}ms with error: ${e.getMessage}"
+      }
     }
 
     Thread.sleep(250)
@@ -209,11 +236,28 @@ class ImageApiUploader(maxUploads:Int = 1, imageMetaFile: String, licensesFile: 
     (ImageOrigin.apply _).tupled(x)
   }
 
-  def toImageAuthor(line: String): ImageAuthor = {
-    val x = line.split("#!#") match {
-      case Array(a, b, c) => (a, b, c)
+  def toImageAuthor(line: String): Option[ImageAuthor] = {
+    line.split("#!#") match {
+      case Array(a, b, c) => {
+        if(c == "NULL") None else Option((ImageAuthor.apply _).tupled((a,replaceNULLWithEmtpy(b),c)))
+      }
+      case _ => None
     }
-    (ImageAuthor.apply _).tupled(x)
+  }
+
+  def replaceNULLWithEmtpy(str: String): String = {
+    if(str == "NULL") "" else str
+  }
+
+  def test() = {
+    val lines = List("145610#!#Fotograf#!#NULL", "145611#!#NULL#!#Kenneth")
+    val imageAuthor = lines
+      .map(line => toImageAuthor(line)).collect{case Some(x) => x}
+      .map(author => author.nid -> author)
+      .groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
+
+
+    imageAuthor.foreach(println)
   }
 }
 
