8962abf035a00150d80d2dfe216fb7c33668e72e
Kenneth Stigen <kes@knowit.no>
2015-10-13 08:51:50 +0200

    NDLA-37: Oppdatert med navn på prod-base

--- src/main/db/createDBUsers.sql
+++ src/main/db/createDBUsers.sql
@@ -14,18 +14,18 @@
 
 
 -- READONLY
-CREATE USER imageapi_read with PASSWORD 'Qwf9m7PywTJTrti';
+CREATE USER imageapi_read with PASSWORD '<passord>';
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO imageapi_read;
 
-GRANT CONNECT ON DATABASE ndla_image_api_test to imageapi_read;
+GRANT CONNECT ON DATABASE imagemeta_prod to imageapi_read;
 GRANT USAGE ON SCHEMA public to imageapi_read;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO imageapi_read;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO imageapi_read;
 
 -- WRITE
-CREATE USER imageapi_write with PASSWORD 'cx8QnLj9qEszrep';
+CREATE USER imageapi_write with PASSWORD '<passord>';
 
-GRANT CONNECT ON DATABASE ndla_image_api_test to imageapi_write;
+GRANT CONNECT ON DATABASE imagemeta_prod to imageapi_write;
 GRANT USAGE ON SCHEMA public to imageapi_write;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO imageapi_write;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO imageapi_write;
