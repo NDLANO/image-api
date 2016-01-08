b1efcaf217dcfade085b3978626181c599fedd15
Kenneth Stigen <kes@knowit.no>
2016-01-08 15:27:59 +0100

    NDLA-71: Lagt til output i build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 VERSION="$1"
+source ./build.properties
+PROJECT="$NDLAOrganization/$NDLAComponentName"
+
 if [ -z $VERSION ]
 then
     VERSION="SNAPSHOT"
 fi
 
 sbt -Ddocker.tag=$VERSION docker
+echo "BUILT $PROJECT:$VERSION"
