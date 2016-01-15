90a9e76c9e8109d3c26f93543640590746d32847
Kenneth Stigen <kes@knowit.no>
2016-01-15 12:55:24 +0100

    NDLA-92: Glemte å rydde i kontroller

--- src/main/scala/no/ndla/imageapi/AdminController.scala
+++ src/main/scala/no/ndla/imageapi/AdminController.scala
@@ -1,22 +1,15 @@
 package no.ndla.imageapi
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
 import com.typesafe.scalalogging.LazyLogging
 import no.ndla.imageapi.business.SearchIndexer
-import no.ndla.imageapi.integration.AmazonIntegration
 import no.ndla.imageapi.model.Error
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.{Ok, ScalatraServlet}
 import org.scalatra.json.NativeJsonSupport
+import org.scalatra.{Ok, ScalatraServlet}
 
 class AdminController extends ScalatraServlet with NativeJsonSupport with LazyLogging  {
 
   protected implicit override val jsonFormats: Formats = DefaultFormats
-
-  val meta = AmazonIntegration.getImageMeta()
-  val indexMeta = AmazonIntegration.getIndexMeta()
 
   error{
     case t:Throwable => {
