63dab4ecb5998da785a9b740c423b55b1345873b
Kenneth Stigen <kes@knowit.no>
2016-10-12 16:10:20 +0200

    NDLA-420: Reading secrets from s3 if env other than local

--- src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
+++ src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
@@ -9,43 +9,44 @@
 package no.ndla.imageapi
 
 import com.typesafe.scalalogging.LazyLogging
+import no.ndla.imageapi.Secrets.readSecrets
 
 import scala.collection.mutable
 import scala.io.Source
+import scala.util.{Failure, Properties, Success, Try}
+
 
 object ImageApiProperties extends LazyLogging {
+  object PropertyKeys {
+    val MetaUserNameKey = "META_USER_NAME"
+    val MetaPasswordKey = "META_PASSWORD"
+    val MetaResourceKey = "META_RESOURCE"
+    val MetaServerKey = "META_SERVER"
+    val MetaPortKey = "META_PORT"
+    val MetaSchemaKey = "META_SCHEMA"
+  }
 
   var ImageApiProps: mutable.Map[String, Option[String]] = mutable.HashMap()
 
   lazy val ApplicationPort = getInt("APPLICATION_PORT")
-
-  val CorrelationIdKey = "correlationID"
-  val CorrelationIdHeader = "X-Correlation-ID"
-
-
   lazy val ContactEmail = get("CONTACT_EMAIL")
   lazy val HostAddr = get("HOST_ADDR")
   lazy val Domain = get("DOMAIN")
-  val HealthControllerPath = "/health"
-  val ImageControllerPath = "/images"
   lazy val ImageUrlBase = Domain + ImageControllerPath + "/"
 
-  lazy val MetaUserName = get("META_USER_NAME")
-  lazy val MetaPassword = get("META_PASSWORD")
-  lazy val MetaResource = get("META_RESOURCE")
-  lazy val MetaServer = get("META_SERVER")
-  lazy val MetaPort = getInt("META_PORT")
+  lazy val MetaUserName = get(PropertyKeys.MetaUserNameKey)
+  lazy val MetaPassword = get(PropertyKeys.MetaPasswordKey)
+  lazy val MetaResource = get(PropertyKeys.MetaResourceKey)
+  lazy val MetaServer = get(PropertyKeys.MetaServerKey)
+  lazy val MetaPort = getInt(PropertyKeys.MetaPortKey)
+  lazy val MetaSchema = get(PropertyKeys.MetaSchemaKey)
   lazy val MetaInitialConnections = getInt("META_INITIAL_CONNECTIONS")
   lazy val MetaMaxConnections = getInt("META_MAX_CONNECTIONS")
-  lazy val MetaSchema = get("META_SCHEMA")
 
   lazy val StorageName = get("STORAGE_NAME")
   lazy val StorageAccessKey = get("STORAGE_ACCESS_KEY")
   lazy val StorageSecretKey = get("STORAGE_SECRET_KEY")
 
-  lazy val TopicAPIUrl = get("TOPIC_API_URL")
-
-  val SearchHost = "search-engine"
   lazy val SearchServer = get("SEARCH_SERVER")
   lazy val SearchRegion = get("SEARCH_REGION")
   lazy val SearchIndex = get("SEARCH_INDEX")
@@ -55,26 +56,25 @@ object ImageApiProperties extends LazyLogging {
   lazy val MaxPageSize: Int = getInt("SEARCH_MAX_PAGE_SIZE")
   lazy val IndexBulkSize = getInt("INDEX_BULK_SIZE")
 
+  lazy val TopicAPIUrl = get("TOPIC_API_URL")
   lazy val MigrationHost = get("MIGRATION_HOST")
   lazy val MigrationUser = get("MIGRATION_USER")
   lazy val MigrationPassword = get("MIGRATION_PASSWORD")
 
+  val CorrelationIdKey = "correlationID"
+  val CorrelationIdHeader = "X-Correlation-ID"
+  val HealthControllerPath = "/health"
+  val ImageControllerPath = "/images"
   val MappingHost = "mapping-api"
   val IsoMappingCacheAgeInMs = 1000 * 60 * 60 // 1 hour caching
   val LicenseMappingCacheAgeInMs = 1000 * 60 * 60 // 1 hour caching
 
-  def verify() = {
-    val missingProperties = ImageApiProps.filter(entry => entry._2.isEmpty).toList
-    if (missingProperties.nonEmpty){
-      missingProperties.foreach(entry => logger.error("Missing required environment variable {}", entry._1))
-
-      logger.error("Shutting down.")
-      System.exit(1)
-    }
-  }
-
   def setProperties(properties: Map[String, Option[String]]) = {
-    properties.foreach(prop => ImageApiProps.put(prop._1, prop._2))
+    val missingProperties = properties.filter(_._2.isEmpty).keys
+    missingProperties.isEmpty match {
+      case true => Success(properties.foreach(prop => ImageApiProps.put(prop._1, prop._2)))
+      case false => Failure(new RuntimeException(s"Missing required properties: ${missingProperties.mkString(", ")}"))
+    }
   }
 
   private def get(envKey: String): String = {
@@ -91,18 +91,25 @@ object ImageApiProperties extends LazyLogging {
   private def getBoolean(envKey: String): Boolean = {
     get(envKey).toBoolean
   }
-
 }
 
-object PropertiesLoader {
+object PropertiesLoader extends LazyLogging {
   val EnvironmentFile = "/image-api.env"
 
-  def readPropertyFile(): Map[String,Option[String]] = {
-    Source.fromInputStream(getClass.getResourceAsStream(EnvironmentFile)).getLines().map(key => key -> scala.util.Properties.envOrNone(key)).toMap
+  def readPropertyFile() = {
+    Try(Source.fromInputStream(getClass.getResourceAsStream(EnvironmentFile)).getLines().map(key => key -> Properties.envOrNone(key)).toMap)
   }
 
   def load() = {
-    ImageApiProperties.setProperties(readPropertyFile())
-    ImageApiProperties.verify()
+    val verification = for {
+      file <- readPropertyFile()
+      secrets <- readSecrets()
+      didSetProperties <- ImageApiProperties.setProperties(file ++ secrets)
+    } yield didSetProperties
+
+    if(verification.isFailure){
+      logger.error("Unable to load properties", verification.failed.get)
+      System.exit(1)
+    }
   }
 }
