ddd2d39473d77bfaae5ab5ee2e234ad7d2cf485a
Henrik Nårstad <henrik.narstad@knowit.no>
2016-12-19 13:54:13 +0100

    add test for getISO639 #143

---
+++ src/test/scala/no/ndla/imageapi/service/TagsServiceTest.scala
@@ -0,0 +1,28 @@
+/*
+ * Part of NDLA image_api.
+ * Copyright (C) 2016 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.imageapi.service
+
+import no.ndla.imageapi.{TestEnvironment, UnitSuite}
+
+class TagsServiceTest extends UnitSuite with TestEnvironment {
+  val service = new TagsService
+
+  test("getISO639 returns a iso639 language code for a valid language url") {
+    service.getISO639("http://psi.some.url.org/#nob") should equal (Option("nb"))
+    service.getISO639("http://psi.some.url.org/#nno") should equal (Option("nn"))
+    service.getISO639("http://psi.some.url.org/#eng") should equal (Option("en"))
+  }
+
+  test("getISO639 returns None for an invalid language url") {
+    service.getISO639("http://psi.some.url.org/#XXX") should equal (None)
+    service.getISO639("http://psi.some.url.org/#YYY") should equal (None)
+    service.getISO639("http://psi.some.url.org/#ZZZ") should equal (None)
+  }
+
+}
