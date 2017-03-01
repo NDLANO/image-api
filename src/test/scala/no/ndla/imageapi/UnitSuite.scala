048bfc7cf5d7206902d1af4b7423efb64f7ce4e7
Tanja Gruschke <tmg@knowit.no>
2017-03-01 16:21:49 +0100

    #220: index name prop name change

--- src/test/scala/no/ndla/imageapi/UnitSuite.scala
+++ src/test/scala/no/ndla/imageapi/UnitSuite.scala
@@ -27,7 +27,7 @@ abstract class UnitSuite extends FunSuite with Matchers with OptionValues with I
   setEnv("MIGRATION_HOST", "some-host")
   setEnv("MIGRATION_USER", "some-user")
   setEnv("MIGRATION_PASSWORD", "some-password")
-  setEnv("SEARCH_ALIAS", "integration-test-index")
+  setEnv("SEARCH_INDEX_NAME", "integration-test-index")
 
 
   def setEnv(key: String, value: String) = {
