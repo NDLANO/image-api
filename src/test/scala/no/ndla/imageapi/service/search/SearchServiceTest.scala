0d4cf6dbc1a767d2cb747a9bc9686de7bcb9fb24
Runar Furenes <ruf@knowit.no>
2017-10-10 12:40:08 +0200

    #72: Tests for empty index creation GlobalDigitalLibraryio/issues#72

---
+++ src/test/scala/no/ndla/imageapi/service/search/SearchServiceTest.scala
@@ -0,0 +1,32 @@
+/*
+ * Part of NDLA image_api.
+ * Copyright (C) 2017 NDLA
+ *
+ * See LICENSE
+ */
+
+package no.ndla.imageapi.service.search
+
+import no.ndla.imageapi.{TestEnvironment, UnitSuite}
+import org.mockito.Mockito._
+
+import scala.util.Success
+
+class SearchServiceTest extends UnitSuite with TestEnvironment {
+
+  override val searchService = new SearchService
+
+  test("That createEmptyIndexIfNoIndexesExist never creates empty index if an index already exists") {
+    when(indexService.findAllIndexes()).thenReturn(Success(Seq("index1")))
+    searchService.createEmptyIndexIfNoIndexesExist()
+    verify(indexBuilderService, never()).createEmptyIndex
+  }
+
+  test("That createEmptyIndexIfNoIndexesExist creates empty index if no indexes already exists") {
+    when(indexService.findAllIndexes()).thenReturn(Success(List.empty))
+    when(indexBuilderService.createEmptyIndex).thenReturn(Success())
+    searchService.createEmptyIndexIfNoIndexesExist()
+    verify(indexBuilderService).createEmptyIndex
+  }
+
+}
