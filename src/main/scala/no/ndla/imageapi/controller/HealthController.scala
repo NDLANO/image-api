ff5df90e6c846d1383af60554ccb887ff1dee1e2
Jonas Natten <jonas.natten@knowit.no>
2017-10-12 15:32:28 +0200

    #675: /health checks if images are loaded correctly

--- src/main/scala/no/ndla/imageapi/controller/HealthController.scala
+++ src/main/scala/no/ndla/imageapi/controller/HealthController.scala
@@ -8,7 +8,12 @@
 
 package no.ndla.imageapi.controller
 
-import org.scalatra.{Ok, ScalatraServlet}
+import no.ndla.imageapi.ImageApiProperties
+import org.scalatra.{InternalServerError, Ok, ScalatraServlet}
+
+import scalaj.http.{Http, HttpOptions}
+import org.json4s._
+import org.json4s.native.JsonParser
 
 trait HealthController {
   val healthController: HealthController
@@ -16,7 +21,24 @@ trait HealthController {
   class HealthController extends ScalatraServlet {
 
     get("/") {
-      Ok()
+      implicit val formats = DefaultFormats
+
+      val searchResponse = Http(s"http://0.0.0.0:${ImageApiProperties.ApplicationPort}${ImageApiProperties.ImageApiBasePath}/v2/images/").asString
+      searchResponse.code match {
+        case 200 =>
+        case _ => InternalServerError()
+      }
+
+      val searchJson = JsonParser.parse(searchResponse.body).extract[no.ndla.imageapi.model.api.SearchResult]
+      val imageUrl = searchJson.results.head.metaUrl
+
+      if(searchJson.totalCount == 0)
+        Ok()
+
+      Http(imageUrl).asString.code match {
+        case 200 => Ok()
+        case _ => InternalServerError()
+      }
     }
   }
 }
