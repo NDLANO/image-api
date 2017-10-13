cf247602621fb6c760ae6d0f4ebf99ff59d1f30c
Jonas Natten <jonas.natten@knowit.no>
2017-10-13 08:59:55 +0200

    #675: health check refactored for simpler testing

--- src/main/scala/no/ndla/imageapi/controller/HealthController.scala
+++ src/main/scala/no/ndla/imageapi/controller/HealthController.scala
@@ -9,34 +9,46 @@
 package no.ndla.imageapi.controller
 
 import no.ndla.imageapi.ImageApiProperties
-import org.scalatra.{InternalServerError, Ok, ScalatraServlet}
-
-import scalaj.http.{Http, HttpOptions}
+import no.ndla.imageapi.model.api.SearchResult
 import org.json4s._
 import org.json4s.native.JsonParser
+import org.scalatra.{ActionResult, InternalServerError, Ok, ScalatraServlet}
+
+import scalaj.http.{Http, HttpResponse}
 
 trait HealthController {
   val healthController: HealthController
 
   class HealthController extends ScalatraServlet {
 
-    get("/") {
-      implicit val formats = DefaultFormats
+    implicit val formats = DefaultFormats
 
-      val searchResponse = Http(s"http://0.0.0.0:${ImageApiProperties.ApplicationPort}${ImageApiProperties.ImageApiBasePath}/v2/images/").asString
-      searchResponse.code match {
-        case 200 =>
+    def getImageSearchResponse(url: String): HttpResponse[String] = {
+      Http(url).asString
+    }
+
+    def getImageUrl(response: HttpResponse[String]): (String, Long) = {
+      val json = JsonParser.parse(response.body).extract[SearchResult]
+      (json.results.head.metaUrl,json.totalCount)
+    }
+
+    def getReturnCode(imageResponse: HttpResponse[String]): ActionResult = {
+      imageResponse.code match {
+        case 200 => Ok()
         case _ => InternalServerError()
       }
+    }
 
-      val searchJson = JsonParser.parse(searchResponse.body).extract[no.ndla.imageapi.model.api.SearchResult]
-      val imageUrl = searchJson.results.head.metaUrl
+    get("/") {
+      val searchResponse = getImageSearchResponse(
+        s"http://0.0.0.0:${ImageApiProperties.ApplicationPort}${ImageApiProperties.ImageApiBasePath}/v2/images/")
+      val (imageUrl,totalCount) = getImageUrl(searchResponse)
 
-      if(searchJson.totalCount == 0)
+      if(totalCount == 0)
         Ok()
 
-      Http(imageUrl).asString.code match {
-        case 200 => Ok()
+      searchResponse.code match {
+        case 200 => getReturnCode(Http(imageUrl).asString)
         case _ => InternalServerError()
       }
     }
--- src/test/scala/no/ndla/imageapi/controller/HealthControllerTest.scala
+++ src/test/scala/no/ndla/imageapi/controller/HealthControllerTest.scala
@@ -8,18 +8,71 @@
 
 package no.ndla.imageapi.controller
 
-import no.ndla.imageapi.{ImageApiProperties, TestEnvironment, UnitSuite}
+import no.ndla.imageapi.{TestEnvironment, UnitSuite}
+import org.mockito.Mockito.when
 import org.scalatra.test.scalatest.ScalatraFunSuite
+import org.scalatra.{InternalServerError, Ok}
+
+import scalaj.http.HttpResponse
 
 class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraFunSuite {
 
   lazy val controller = new HealthController
-  addServlet(controller, ImageApiProperties.HealthControllerPath)
 
-  test("That /health returns 200 ok") {
-    get("/health") {
-      status should equal (200)
-    }
+  val imageSearchBody = s"""{
+                           |  "totalCount": 1,
+                           |  "page": 1,
+                           |  "pageSize": 10,
+                           |  "results": [
+                           |    {
+                           |      "id": "1",
+                           |      "title": {
+                           |        "title": "Meterolog Kristian Trægde i 1986",
+                           |        "language": "unknown"
+                           |      },
+                           |      "altText": {
+                           |        "alttext": "",
+                           |        "language": "nb"
+                           |      },
+                           |      "previewUrl": "http://0.0.0.0/image-api/raw/sx873733_1.jpg",
+                           |      "metaUrl": "http://0.0.0.0/image-api/v2/images/1",
+                           |      "license": "by-nc-sa"
+                           |    }
+                           |  ]
+                           |}""".stripMargin
+
+  test("that getReturnCode returns 200 when response is okay") {
+    val searchResponse = mock[HttpResponse[String]]
+    when(searchResponse.code).thenReturn(200)
+    when(searchResponse.body).thenReturn(imageSearchBody)
+    val imageResponse = controller.getReturnCode(searchResponse)
+
+    imageResponse should equal(Ok())
   }
 
+  test("that getReturnCode returns InternalServerError when response is incorrect") {
+    val imageNotFoundBody = """{
+                          |	"code": "NOT FOUND",
+                          |	"description": "Image with id 3 and language None not found",
+                          |	"occuredAt": "2017-10-13 08:48:58.999"
+                          |}""".stripMargin
+
+    val searchResponse = mock[HttpResponse[String]]
+    when(searchResponse.code).thenReturn(404)
+    when(searchResponse.body).thenReturn(imageNotFoundBody)
+    val imageResponse = controller.getReturnCode(searchResponse)
+
+    imageResponse should equal(InternalServerError())
+  }
+
+  test("that url is fetched properly") {
+    val searchResponse = mock[HttpResponse[String]]
+    when(searchResponse.code).thenReturn(200)
+    when(searchResponse.body).thenReturn(imageSearchBody)
+    val expectedUrl = "http://0.0.0.0/image-api/v2/images/1"
+    val (url, totalCount) = controller.getImageUrl(searchResponse)
+
+    url should equal(expectedUrl)
+    totalCount should equal(1)
+  }
 }
