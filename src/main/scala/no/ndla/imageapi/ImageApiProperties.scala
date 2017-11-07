08545df78d8ee047c5c5c006a4b4ce8ea9476b1e
Jonas Natten <jonas.natten@knowit.no>
2017-11-07 13:10:22 +0100

    #747: Adds allowableValues to author api-model

--- src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
+++ src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
@@ -34,14 +34,15 @@ object ImageApiProperties extends LazyLogging {
 
   val DefaultLanguage = "nb"
 
-  val oldCreatorTypes = List("opphavsmann", "fotograf", "kunstner", "redaksjonelt", "forfatter", "manusforfatter", "innleser", "oversetter", "regissør", "illustratør", "medforfatter", "komponist")
-  val creatorTypes = List("originator", "photographer", "artist", "editorial", "writer", "scriptwriter", "reader", "translator", "director", "illustrator", "cowriter", "composer")
+  final val oldCreatorTypes = List("opphavsmann", "fotograf", "kunstner", "redaksjonelt", "forfatter", "manusforfatter", "innleser", "oversetter", "regissør", "illustratør", "medforfatter", "komponist")
+  final val creatorTypes = List("originator", "photographer", "artist", "editorial", "writer", "scriptwriter", "reader", "translator", "director", "illustrator", "cowriter", "composer")
 
-  val oldProcessorTypes = List("bearbeider", "tilrettelegger", "redaksjonelt", "språklig", "ide", "sammenstiller", "korrektur")
-  val processorTypes = List("processor", "facilitator", "editorial", "linguistic", "idea", "compiler", "correction")
+  final val oldProcessorTypes = List("bearbeider", "tilrettelegger", "redaksjonelt", "språklig", "ide", "sammenstiller", "korrektur")
+  final val processorTypes = List("processor", "facilitator", "editorial", "linguistic", "idea", "compiler", "correction")
 
-  val oldRightsholderTypes = List("rettighetshaver", "forlag", "distributør", "leverandør")
-  val rightsholderTypes = List("rightsholder", "publisher", "distributor", "supplier")
+  final val oldRightsholderTypes = List("rettighetshaver", "forlag", "distributør", "leverandør")
+  final val rightsholderTypes = List("rightsholder", "publisher", "distributor", "supplier")
+  final val authorTypeString = (ImageApiProperties.creatorTypes ++ ImageApiProperties.processorTypes ++ ImageApiProperties.rightsholderTypes).mkString(",")
 
   val IsoMappingCacheAgeInMs = 1000 * 60 * 60 // 1 hour caching
   val LicenseMappingCacheAgeInMs = 1000 * 60 * 60 // 1 hour caching
