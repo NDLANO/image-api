617f49f80ce7758e16a17620dba1c1f0681e0a43
Henrik Nårstad <henrik.narstad@knowit.no>
2018-04-05 14:56:25 +0200

    fix test #964

--- src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
+++ src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
@@ -37,7 +37,7 @@ object ImageApiProperties extends LazyLogging {
   val DefaultLanguage = "nb"
 
   val oldCreatorTypes = List("opphavsmann", "fotograf", "kunstner", "forfatter", "manusforfatter", "innleser", "oversetter", "regissør", "illustratør", "medforfatter", "komponist")
-  val creatorTypes = List("originator", "photographer", "artist", "editorial", "writer", "scriptwriter", "reader", "translator", "director", "illustrator", "cowriter", "composer")
+  val creatorTypes = List("originator", "photographer", "artist", "writer", "scriptwriter", "reader", "translator", "director", "illustrator", "cowriter", "composer")
 
   val oldProcessorTypes = List("bearbeider", "tilrettelegger", "redaksjonelt", "språklig", "ide", "sammenstiller", "korrektur")
   val processorTypes = List("processor", "facilitator", "editorial", "linguistic", "idea", "compiler", "correction")
