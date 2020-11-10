71abd7513211800c9cc37f4468106e8ee9fd8776
Jonas Natten <jonas.natten@knowit.no>
2020-11-10 10:46:23 +0100

    #2325: Upgrade flyway to 7.1.1

--- src/main/scala/no/ndla/imageapi/DBMigrator.scala
+++ src/main/scala/no/ndla/imageapi/DBMigrator.scala
@@ -10,10 +10,11 @@ package no.ndla.imageapi
 
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.output.MigrateResult
 
 object DBMigrator {
 
-  def migrate(datasource: HikariDataSource): Int = {
+  def migrate(datasource: HikariDataSource): MigrateResult = {
     val flyway = Flyway
       .configure()
       .table("schema_version") // Flyway's default table name changed, so we specify the old one.
