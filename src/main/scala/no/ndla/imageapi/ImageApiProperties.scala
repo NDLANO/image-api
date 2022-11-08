6ecf6bdf480d112e21ee2df78c4677791ed878ea
ghveem <guttorm@hveem.no>
2022-11-08 14:32:33 +0100

    Update ImageApiProperties.scala
    support-til-hjelp

--- src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
+++ src/main/scala/no/ndla/imageapi/ImageApiProperties.scala
@@ -28,7 +28,7 @@ object ImageApiProperties extends LazyLogging {
   val DefaultLanguage: String = propOrElse("DEFAULT_LANGUAGE", "nb")
   val ContactName: String = propOrElse("CONTACT_NAME", "NDLA")
   val ContactUrl: String = propOrElse("CONTACT_URL", "ndla.no")
-  val ContactEmail: String = propOrElse("CONTACT_EMAIL", "support+api@ndla.no")
+  val ContactEmail: String = propOrElse("CONTACT_EMAIL", "hjelp+api@ndla.no")
   val TermsUrl: String = propOrElse("TERMS_URL", "https://om.ndla.no/tos")
 
   val CorrelationIdKey = "correlationID"
